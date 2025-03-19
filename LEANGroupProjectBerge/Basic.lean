a5caa6835a14b40401b9bdffcb31d980c6872e40
Ayşe Başak Kaya <sc20abk@leeds.ac.uk>
2025-03-19 17:12:23 +0000

    definitions added with Kitu

--- LEANGroupProjectBerge/Basic.lean
+++ LEANGroupProjectBerge/Basic.lean
@@ -10,24 +10,29 @@ import Mathlib.Combinatorics.SimpleGraph.Path
 import Mathlib.Combinatorics.SimpleGraph.Subgraph
 import Mathlib.Combinatorics.SimpleGraph.Matching
 
+import Mathlib.Combinatorics.SimpleGraph.Connectivity.Subgraph
+import Mathlib.Algebra.Group.Even
+
+
 namespace SimpleGraph
 
 universe u
 variable {V : Type u}
 variable {G : SimpleGraph V}
+variable {F : SimpleGraph V}
 variable {M : G.Subgraph}
-variable {u v w: V}
+variable {u v w x : V}
 
 
 namespace Walk
 
-structure IsAlternatingCycle {F : SimpleGraph V} {u : V} (p : F.Walk u u) (M : G.Subgraph) extends p.IsCycle : Prop where
+structure IsAlternatingCycle (p : F.Walk u u) (M : G.Subgraph) extends p.IsCycle : Prop where
   alternates : ∀ ⦃w x y: V⦄, w ≠ y → s(w,x) ∈ p.edges → s(x,y) ∈ p.edges → (M.Adj w x ↔ ¬M.Adj x y)
 
-structure IsAlternatingPath {F : SimpleGraph V} {u v : V} (p : F.Walk u v) (M : G.Subgraph) extends p.IsPath : Prop where
+structure IsAlternatingPath (p : F.Walk u v) (M : G.Subgraph) extends p.IsPath : Prop where
   alternates : ∀ ⦃w x y: V⦄, w ≠ y → s(w,x) ∈ p.edges → s(x,y) ∈ p.edges → (M.Adj w x ↔ ¬M.Adj x y)
 
-structure IsAugmentingPath {F : SimpleGraph V} {u v : V} (p : F.Walk u v) (M : G.Subgraph) extends p.IsAlternatingPath M : Prop where
+structure IsAugmentingPath (p : F.Walk u v) (M : G.Subgraph) extends p.IsAlternatingPath M : Prop where
   ends_unsaturated : u≠v ∧ u ∉ M.support ∧ v ∉ M.support
 
 end Walk
@@ -74,15 +79,85 @@ def saturatedSubgraph (M : G.Subgraph) : G.Subgraph where
 end Subgraph
 
 
+namespace ConnectedComponent
 
+
+-- adjacency and edge set of connected components
+def Adj (c : F.ConnectedComponent) (u v : V) : Prop :=
+  u ∈ c.supp ∧ v ∈ c.supp ∧ F.Adj u v
+
+
+theorem component_adj_symm (c : F.ConnectedComponent) : Symmetric c.Adj := by
+  intros x y h
+  obtain ⟨ h_xc, h_yc, h_f ⟩ := h
+  exact ⟨ h_yc, h_xc , F.adj_symm h_f ⟩
+
+
+def edgeSet (c : F.ConnectedComponent) : Set (Sym2 V) :=
+  Sym2.fromRel c.component_adj_symm
+
+
+end ConnectedComponent
+
+namespace Walk
+
+def vertexSet (p : F.Walk u v) : Set V :=
+  {v : V | v ∈ p.support}
+
+def edgeSet (p : F.Walk u v) : Set (Sym2 V) :=
+  {e : Sym2 V | e ∈ p.edges}
+
+end Walk
+
+namespace Walk
+
+structure IsAltCycleComponent (p : F.Walk u u) (M : G.Subgraph) (c : F.ConnectedComponent) extends p.IsAlternatingCycle M : Prop where
+  vertices_eq : c.supp = p.vertexSet
+  edges_eq : c.edgeSet = p.edgeSet
+
+
+structure IsAltPathComponent (p : F.Walk u v) (M : G.Subgraph) (c : F.ConnectedComponent) extends p.IsAlternatingPath M : Prop where
+  vertices_eq : c.supp = p.vertexSet
+  edges_eq : c.edgeSet = p.edgeSet
+
+
+end Walk
 
 namespace ConnectedComponent
 
-def componentAltCycle {F : SimpleGraph V} (c : F.ConnectedComponent) (M : G.Subgraph) : Prop :=
-  ∃ (u : V) (p : F.Walk u u), p.IsAlternatingCycle M ∧ ∀x:V, x ∈ c.supp → x ∈ p.support
+-- whether a component is equivalent to a alternating path or cycle
+def componentAltCycle (c : F.ConnectedComponent) (M : G.Subgraph) : Prop :=
+  ∃ (u : V) (p : F.Walk u u), p.IsAltCycleComponent M c
 
 
-def componentAltPath {F : SimpleGraph V} (c : F.ConnectedComponent) (M : G.Subgraph) : Prop :=
-  ∃ (u v : V) (p : F.Walk u v), p.IsAlternatingPath M ∧ ∀x:V, x ∈ c.supp → x ∈ p.support
+def componentAltPath (c : F.ConnectedComponent) (M : G.Subgraph) : Prop :=
+  ∃ (u v : V) (p : F.Walk u v), p.IsAltPathComponent M c
 
 end ConnectedComponent
+
+
+
+theorem cycle_is_even(M' : G.Subgraph) (hm : M.IsMatching) (hm' : M'.IsMatching):
+∀ (p : (symmDiff M.spanningCoe M'.spanningCoe).Walk u u ), p.IsCycle → Even p.edgeSet.encard := by
+  let F := (symmDiff M.spanningCoe M'.spanningCoe)
+  sorry
+
+theorem component_is_alt (M' : G.Subgraph) (hm : M.IsMatching) (hm' : M'.IsMatching):
+∀(cc : (symmDiff M.spanningCoe M'.spanningCoe).ConnectedComponent), (cc.componentAltPath M) ∨ (cc.componentAltCycle M):=
+  sorry
+
+theorem cycle_eq_in_symm_diff(M' : G.Subgraph) (hm : M.IsMatching) (hm' : M'.IsMatching):
+∀ (p : (symmDiff M.spanningCoe M'.spanningCoe).Walk u u ), p.IsCycle → (p.edgeSet ∩ M.edgeSet).ncard = (p.edgeSet ∩ M'.edgeSet).ncard := by
+  let F := (symmDiff M.spanningCoe M'.spanningCoe)
+  sorry
+
+theorem component_edges_greater_than (M' : G.Subgraph) (hm : M.IsMatching) (hm' : M'.IsMatching) (he: M'.edgeSet.ncard > M.edgeSet.ncard):
+∃ (cc: (symmDiff M.spanningCoe M'.spanningCoe).ConnectedComponent), (cc.componentAltPath M) ∧ ((cc.edgeSet ∩ M'.edgeSet).ncard > (cc.edgeSet ∩ M.edgeSet).ncard):=
+  sorry
+
+theorem exists_aug_path (M' : G.Subgraph) (hm : M.IsMatching) (hm' : M'.IsMatching):
+∃ (p: (symmDiff M.spanningCoe M'.spanningCoe).Walk u v), (p.IsAugmentingPath M):=
+sorry
+
+theorem berge_left (M : G.Subgraph): IsMaximumMatching M → ¬∃ u v: V, ∃ p: G.Walk u v, p.IsAugmentingPath M :=
+  sorry
