31a5f4d58968d16d5e57a2d8e0f70972a8eef310
oscardwbryan <sc21odwb@leeds.ac.uk>
2025-03-31 23:02:32 +0100

    partition proof halfway done

--- LEANGroupProjectBerge/ComponentPartitioning.lean
+++ LEANGroupProjectBerge/ComponentPartitioning.lean
@@ -61,9 +61,36 @@ theorem comp_edgesets_partition : IsPartition G.edgeSetsPartition := by
     exists E'
     simp
     constructor
-    · sorry
+    · constructor
+      · sorry
+      · have : y ∈ c.supp := (SimpleGraph.ConnectedComponent.mem_supp_congr_adj c h_e).mp rfl
+        have : s(x,y) ∈ c.edgeSet := ⟨ by aesop, this, h_e ⟩
+        aesop
     · intro D h_DP h_eD
-      sorry
+      obtain ⟨ ⟨ c', h_c'⟩, _⟩ := h_DP
+      have h_ec' : s(x,y) ∈ c'.edgeSet := by
+        rw[←h_c']
+        exists ⟨s(x,y), h_e⟩
+      have : c=c' := by
+        obtain ⟨_, _, _⟩ := h_ec'
+        aesop
+      apply Set.ext; rintro ⟨a, h_a⟩; revert a; apply Sym2.ind; intro u v h_a
+      constructor
+      · intro h_aD
+        have h_ac': s(u,v) ∈ c'.edgeSet := by
+          have : s(u,v) ∈ Subtype.val '' D := by
+            exists ⟨s(u,v), h_a⟩
+          aesop
+        rw[←this] at h_ac'
+        assumption
+      · intro h_aE
+        have : s(u,v) ∈ c'.edgeSet := by aesop
+        rw[←h_c'] at this
+        unfold Set.image at this
+        obtain ⟨⟨a', h_a'⟩, h_a'D, h_aa'⟩ := this
+        have : a'=s(u,v) := by aesop
+        aesop
+
 
 
 
