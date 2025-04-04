94ed8da5f8d41e88da66eb2faebbe4caf244d50a
Lauren Thompson <sc21lkt@leeds.ac.uk>
2025-04-04 20:15:49 +0100

    no more sorrys

--- LEANGroupProjectBerge/symmDiffIsMatching.lean
+++ LEANGroupProjectBerge/symmDiffIsMatching.lean
@@ -5,8 +5,13 @@ import Mathlib.Combinatorics.SimpleGraph.Subgraph
 import Mathlib.Combinatorics.SimpleGraph.Matching
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.Subgraph
 
-import LEANGroupProjectBerge.Basic
+import Mathlib.Order.SymmDiff
+import Mathlib.Data.Set.Basic
+import Mathlib.Data.Set.SymmDiff
+import Mathlib.Logic.ExistsUnique
 
+import LEANGroupProjectBerge.Basic
+import LEANGroupProjectBerge.outsideOfPath
 
 
 namespace SimpleGraph
@@ -15,10 +20,125 @@ universe u
 variable {V : Type u}
 variable {G : SimpleGraph V}
 variable {M : G.Subgraph}
-variable {u v w x: V}
+variable {u v w x y: V}
+
+--Apologies for having every assumption in () not {}, I needed everything explicit whilst writing otherwise I got confused.
 
 open Walk
 
+open Classical
 
-lemma symmDiff_M_p_is_matching (M : G.Subgraph) (u v : V ) (p: G.Walk u v) (h1: M.IsMatching) (h2: p.IsAugmentingPath M): (symmDiff M p.toSubgraph).IsMatching :=
+--Placeholder from Josh's node
+lemma AugPathUniqueNeighbourInAugPath {M :G.Subgraph}{p: G.Walk u v}[Finite V]
+(h1: M.IsMatching)(h2: p.IsAugmentingPath M) :
+∀w : V, w∈p.support → ∃! w',(symmDiff M.spanningCoe p.toSubgraph.spanningCoe).Adj w w' := by
 sorry
+
+--p.support is ordered list, everything else is set
+--Converting Josh's node to a form that matches what I had.
+lemma AugPathUniqueNeighbourInAugPathRewrite {M':SimpleGraph V} {M :G.Subgraph}{p: G.Walk u v}[Finite V]
+(h1: M.IsMatching)(h2: p.IsAugmentingPath M) (h3:M' = (symmDiff M.spanningCoe p.toSubgraph.spanningCoe)) :
+∀w : V, w∈p.toSubgraph.support → ∃! w',M'.Adj w w' := by
+  intro w
+  exact
+    fun h4 : w∈p.toSubgraph.support =>
+    have h9: p.toSubgraph.support ⊆ p.toSubgraph.verts := (p.toSubgraph).support_subset_verts
+    have h8: w ∈ p.toSubgraph.verts := by aesop
+    have h5: w ∈ p.support := Iff.mp (p.mem_verts_toSubgraph) h8
+    have h6: ∃! w',(symmDiff M.spanningCoe p.toSubgraph.spanningCoe).Adj w w' := AugPathUniqueNeighbourInAugPath h1 h2 w h5
+    have h7:  ∃! w',M'.Adj w w' := by aesop
+    h7
+
+
+
+lemma m'_is_matching_condition [Finite V] (M : G.Subgraph) (M': SimpleGraph V) (u v : V ) (p: G.Walk u v) (h1: M.IsMatching) (h2: p.IsAugmentingPath M) (h3: M' = (symmDiff M.spanningCoe p.toSubgraph.spanningCoe)): ∀ x:V, x ∈ M'.support → ∃! y:V, M'.Adj x y := by
+intro x h
+exact
+  have h4:x ∈ p.toSubgraph.support ∨ x ∈ (M'.support \ p.toSubgraph.support):= (x_in_path_or_not M M' u v x p h3) h
+  show ∃! y:V, M'.Adj x y by
+    cases h4 with
+    | inl hl => exact AugPathUniqueNeighbourInAugPathRewrite h1 h2 h3 x hl
+    | inr hr => exact outside_path_unique_neighbour M M' u v p h1 h3 x hr
+
+
+
+lemma m'_is_subgraph_of_g_hl [Finite V] (M:G.Subgraph) (x y:V) (h:M.spanningCoe.Adj x y): G.Adj x y :=
+  have h1: M.Adj x y := by aesop
+  have h2: G.Adj x y := M.adj_sub h1
+  h2
+
+lemma m'_is_subgraph_of_g_hr [Finite V] (u v x y: V ) (p: G.Walk u v) (h: p.toSubgraph.spanningCoe.Adj x y) : G.Adj x y :=
+  have h1: p.toSubgraph.Adj x y := by aesop
+  have h2: G.Adj x y := p.toSubgraph.adj_sub h1
+  h2
+
+lemma m'_is_subgraph_of_g [Finite V] (M : G.Subgraph) (M': SimpleGraph V) (u v : V ) (p: G.Walk u v)  (h3: M' = (symmDiff M.spanningCoe p.toSubgraph.spanningCoe)): M' ≤ G  := by
+  intro x y
+  exact
+    fun h4: M'.Adj x y =>
+    have h5: s(x,y) ∈ M'.edgeSet := by aesop
+    have h6: s(x,y) ∈ M.spanningCoe.edgeSet ∧ s(x,y) ∉ p.toSubgraph.spanningCoe.edgeSet ∨ s(x,y) ∈ p.toSubgraph.spanningCoe.edgeSet ∧ s(x,y) ∉ M.spanningCoe.edgeSet := by aesop
+    have h7: s(x,y) ∈ M.spanningCoe.edgeSet ∨ s(x,y) ∈ p.toSubgraph.spanningCoe.edgeSet := by aesop
+    have h8: M.spanningCoe.Adj x y ∨ p.toSubgraph.spanningCoe.Adj x y := by aesop
+    have h9: G.Adj x y :=
+      show G.Adj x y by
+        cases h8 with
+        | inl hl => exact m'_is_subgraph_of_g_hl M x y hl
+        | inr hr => exact m'_is_subgraph_of_g_hr u v x y p hr
+    h9
+
+
+
+
+--Induce M'.support removes isolated vertices from toSubgraph
+lemma xy_adj_in_m'_and_hm [Finite V] (M hM : G.Subgraph) (M': SimpleGraph V) (u v x y : V ) (p: G.Walk u v) (h3: M' = (symmDiff M.spanningCoe p.toSubgraph.spanningCoe))(h5: hM = (toSubgraph M' (m'_is_subgraph_of_g M M' u v p h3)).induce M'.support): M'.Adj x y ↔ hM.Adj x y := {
+        mp :=
+          fun h12: M'.Adj x y =>
+          have h13: (toSubgraph M' (m'_is_subgraph_of_g M M' u v p h3)).Adj x y := by aesop
+          have h14: x∈ M'.support :=
+            have h15: ∃y:V, M'.Adj x y := by aesop
+            have h16: x∈ M'.support := Iff.mp M'.mem_support h15
+            h15
+          have h15: y ∈ M'.support :=
+            have h16: M'.Adj y x := Iff.mp (M'.adj_comm x y) h12
+            have h17: ∃x:V, M'.Adj y x:= by aesop
+            have h18: y∈ M'.support := Iff.mp M'.mem_support h17
+            h18
+          have h16: hM.Adj x y := by aesop
+          h16
+        mpr := by aesop
+      }
+
+
+--hM is basically just M' as a subgraph but removing all vertices that don't form edges.
+lemma symmDiff_M_p_is_matching [Finite V] (M hM : G.Subgraph) (M': SimpleGraph V) (u v y : V ) (p: G.Walk u v) (h1: M.IsMatching) (h2: p.IsAugmentingPath M) (h3: M' = (symmDiff M.spanningCoe p.toSubgraph.spanningCoe))(h5: hM = (toSubgraph M' (m'_is_subgraph_of_g M M' u v p h3)).induce M'.support): hM.IsMatching :=
+  have h6: ∀x:V, x ∈ hM.verts → ∃! y, hM.Adj x y := by
+    intro x
+    exact
+      (fun h7: x ∈ hM.verts =>
+      have h8: x ∈ M'.support := by aesop
+      have h9: M'.IsMatchingSimple := by exact m'_is_matching_condition M M' u v p h1 h2 h3
+      have h10: ∃! y, M'.Adj x y := by aesop
+      have h11: M'.Adj x y ↔ hM.Adj x y := {
+        mp :=
+          fun h12: M'.Adj x y =>
+          have h13: (toSubgraph M' (m'_is_subgraph_of_g M M' u v p h3)).Adj x y := by aesop
+          have h14: x∈ M'.support := by aesop
+          have h15: y ∈ M'.support :=
+            have h16: M'.Adj y x := Iff.mp (M'.adj_comm x y) h12
+            have h17: ∃x:V, M'.Adj y x:= by aesop
+            have h18: y∈ M'.support := Iff.mp M'.mem_support h17
+            h18
+          have h16: hM.Adj x y := by aesop
+          h16
+        mpr := by aesop
+      }
+      have h12: ∀y:V, (M'.Adj x y ↔ hM.Adj x y) := by
+        intro y
+        exact (xy_adj_in_m'_and_hm M hM M' u v x y p h3 h5)
+
+      have h13: ∃! y, hM.Adj x y := Iff.mp (existsUnique_congr h12) h10
+      h13
+      )
+  have h7: hM.IsMatching := h6
+  h7
